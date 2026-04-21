d3613dbcbc336e38d9db002dc4539ac748615a8b
Nenasheva Tatiana <nenasheva-tanya05@mail.ru>
2026-04-21 23:59:47 +0300

    update

--- stripe_backend/settings.py
+++ stripe_backend/settings.py
@@ -29,6 +29,7 @@ PUBLISHABLE_KEY = os.getenv('PUBLISHABLE_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
